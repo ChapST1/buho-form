182053629566d58f0c79919fc023faf456a18aac
ChapST1 <cutitox59@gmail.com>
2023-02-13 11:54:09 -0500

    cambiar el cursor si los datos son correctos

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ const inputName = document.getElementById('inputName');
 const inputLabelName = document.getElementById('labelName')
 //random
 const inputRandomNumber = document.getElementById('inputRandom');
+//button
+const btnSubmit = document.getElementById('inputBtn')
+
 
 //eventos
 window.addEventListener('load', () => {
@@ -24,9 +27,15 @@ function onlyText(input) {
         if (/[0-9]/.test(value)) {
             inputName.classList.add('is-invalid');
             inputLabelName.innerHTML = ' ( No se permiten números )';
+
+            btnSubmit.style.cursor = 'not-allowed'
+            btnSubmit.setAttribute('disabled', true)
         } else {
             inputName.classList.remove('is-invalid');
             inputLabelName.innerHTML = 'Nombre';
+
+            btnSubmit.style.cursor = 'pointer'
+            btnSubmit.removeAttribute('disabled')
         }
     })
 }
