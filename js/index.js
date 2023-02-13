11f27a4b186db11b301389f941c04519f7352e00
ChapST1 <cutitox59@gmail.com>
2023-02-13 11:34:13 -0500

    validar solo texto en el campo de Nombre

--- js/index.js
+++ js/index.js
@@ -1,13 +1,33 @@
 //variables
+//name
+const inputName = document.getElementById('inputName');
+const inputLabelName = document.getElementById('labelName')
+//random
 const inputRandomNumber = document.getElementById('inputRandom');
 
 //eventos
 window.addEventListener('load', () => {
     getRandomNumber();
+    onlyText(inputName);
 })
 
 //funciones
 function getRandomNumber() {
     let randomNumber = Math.random().toString().slice(2, 8);
     inputRandomNumber.value = randomNumber;
+}
+function onlyText(input) {
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    input.addEventListener('keyup', (e) => {
+        let value = e.target.value;
+        //usando una expresion regular
+        if (/[0-9]/.test(value)) {
+            inputName.classList.add('is-invalid');
+            inputLabelName.innerHTML = ' ( solo se permiten letras no numeros )';
+        } else {
+            inputName.classList.remove('is-invalid');
+            inputLabelName.innerHTML = 'Nombre';
+        }
+    })
 }
