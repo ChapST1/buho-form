ad725e67c91001800b7df64bf566b883a3f09cce
ChapST1 <cutitox59@gmail.com>
2023-02-13 11:35:07 -0500

    caso 1

--- js/index.js
+++ js/index.js
@@ -17,7 +17,6 @@ function getRandomNumber() {
     inputRandomNumber.value = randomNumber;
 }
 function onlyText(input) {
-    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
     input.addEventListener('keyup', (e) => {
         let value = e.target.value;
