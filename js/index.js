f4c69fb50db526a76a969fa1ff55afdab58f82d5
ChapST1 <cutitox59@gmail.com>
2023-02-18 13:26:07 -0500

    actualizando

--- js/index.js
+++ js/index.js
@@ -1,36 +1,33 @@
 //variables
 
-//modal
-const modal = document.getElementById('modal');
-const btnCancel = document.getElementById('btnCancel');
-const btnAccept = document.getElementById('btnAccept');
-
-//form
-const form = document.getElementById('form');
-
-//name
-const inputName = document.getElementById('inputName');
-const inputLabelName = document.getElementById('labelName')
-//email
-const inputEmail = document.getElementById('inputEmail');
-//random
-const inputRandomNumber = document.getElementById('inputRandom');
-
-//button
-const btnSubmit = document.getElementById('inputBtn')
-
-//Dark Mode
-const btnDarkMode = document.getElementById('darkMode');
+const modalVariables = {
+    modal: document.getElementById('modal'),
+    btnCancel: document.getElementById('btnCancel'),
+    btnAccept: document.getElementById('btnAccept')
+}
+const formVariables = {
+    form: document.getElementById('form'),
+    inputName: document.getElementById('inputName'),
+    inputLabelName: document.getElementById('labelName'),
+    inputEmail: document.getElementById('inputEmail'),
+    inputRandomNumber: document.getElementById('inputRandom'),
+    btnSubmit: document.getElementById('inputBtn')
+}
+const darkVariables = {
+    btnDarkMode: document.getElementById('darkMode')
+}
 
 
 //eventos
 window.addEventListener('load', () => {
     getRandomNumber();
-    onlyText(inputName);
-    validateEmail(inputEmail);
+    onlyText(formVariables);
+    validateEmail(formVariables.inputEmail);
     isActiveDarkMode();
 })
-form.addEventListener('submit', (event) => {
+formVariables.form.addEventListener('submit', (event) => {
+    const { modal, btnCancel, btnAccept } = modalVariables;
+
     event.preventDefault();
     modal.classList.add("modal-is-active")
 
@@ -46,43 +43,43 @@ form.addEventListener('submit', (event) => {
         modal.classList.remove("modal-is-active")
     })
 })
-btnDarkMode.addEventListener('click', () => {
+darkVariables.btnDarkMode.addEventListener('click', (e) => {
     document.body.classList.toggle('body-dark');
     localStorage.setItem("dark-mode", "true")
-    btnDarkMode.firstElementChild.setAttribute('src', './images/sun.png');
-
+    darkVariables.btnDarkMode.firstElementChild.setAttribute('src', './images/sun.png');
     if (!document.body.classList.contains('body-dark')) {
         localStorage.setItem("dark-mode", "false")
-        btnDarkMode.firstElementChild.setAttribute('src', './images/moon.png');
+        darkVariables.btnDarkMode.firstElementChild.setAttribute('src', './images/moon.png');
     }
 })
 //funciones
 function getRandomNumber() {
+    const { inputRandomNumber } = formVariables;
     let randomNumber = Math.random().toString().slice(2, 8);
     let icon = inputRandomNumber.nextElementSibling.style.color = '#8dcd8d';
     inputRandomNumber.value = randomNumber;
 }
-function onlyText(input) {
-    input.addEventListener('keyup', (e) => {
+function onlyText({ inputName, inputLabelName, btnSubmit }) {
+    inputName.addEventListener('keyup', (e) => {
         let value = e.target.value;
         //usando una expresion regular
         if (/[0-9]/.test(value)) {
             inputName.classList.add('is-invalid');
             inputLabelName.innerHTML = 'Nombre  ( Este campo solo acepta letras )';
-            input.nextElementSibling.style.color = '#e95252'
+            inputName.nextElementSibling.style.color = '#e95252'
 
             btnSubmit.style.cursor = 'not-allowed'
             btnSubmit.setAttribute('disabled', true)
         } 
         else if(value.length == 0 ){
-            input.nextElementSibling.style.color = 'transparent'
+            inputName.nextElementSibling.style.color = 'transparent'
             inputLabelName.innerHTML = 'Nombre';
-            input.classList.remove('is-invalid');
+            inputName.classList.remove('is-invalid');
         }
         else {
             inputName.classList.remove('is-invalid');
             inputLabelName.innerHTML = 'Nombre';
-            input.nextElementSibling.style.color = '#8dcd8d';
+            inputName.nextElementSibling.style.color = '#8dcd8d';
 
             btnSubmit.style.cursor = 'pointer'
             btnSubmit.removeAttribute('disabled')
@@ -109,6 +106,7 @@ function validateEmail(email) {
 }
 function isActiveDarkMode() {
     let value = localStorage.getItem("dark-mode");
+    const { btnDarkMode } = darkVariables;
 
     if (value == "true") {
         document.body.classList.add('body-dark');
